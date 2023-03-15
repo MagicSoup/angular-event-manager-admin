ce7420276366505680d61feb46cd172d2b1f1aba
MagicSoup <championlouis@hotmail.com>
2023-03-15 19:43:55 +0100

    feature#1 : add angular testing-librairy

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
   roots: ['<rootDir>/src/'],
-  testMatch: ['**/+(*.)+(spec).+(ts|js)'],
+  testMatch: ['**/+(*.)+(spec|spec.ui).+(ts|js)'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   collectCoverage: true,
   coverageReporters: ['html'],
---
+++ src/app/app.component.spec.ui.ts
@@ -0,0 +1,25 @@
+import {AppComponent} from './app.component';
+import {render, screen} from "@testing-library/angular";
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+  });
+
+  it('angular testing-librairy : should create the app', async () => {
+    // render
+    const {container} = await render(AppComponent, {
+    });
+    // assert
+    expect(container).toBeTruthy();
+  });
+
+  it(`angular testing-librairy : should have as title 'event-manager-webadmin'`,async () => {
+    // render
+    await render(AppComponent, {
+    });
+    // retrieve
+    const title = await screen.getByTestId('title');
+    // assert
+    expect((title as HTMLSpanElement).textContent).toStrictEqual('event-manager-webadmin app is running!');
+  });
+});
